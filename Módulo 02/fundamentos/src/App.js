6802882563bd15acb06e0d3fd182f013a46288d3
Mateus Melo <mateusmelo51@gmail.com>
2023-07-12 19:50:51 -0300

    Add files via upload

---
+++ Módulo 02/fundamentos/src/App.js
@@ -0,0 +1,20 @@
+// Components
+import FirstComponent from './components/FirstComponent';
+
+import TemplateExpressions from './components/TemplateExpressions';
+
+// CSS
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+      <TemplateExpressions/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Módulo 02/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+// Arquivo de estilo
+
+const FirstComponent = () => {
+    // Essa função faz isso
+
+    return (
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+            <p className='teste'>Meu texto</p>
+        </div>
+    )
+}
+
+export default FirstComponent
